2ecd47623f9f2a69dc7facc38446fc184dc37420
Christine Cai <ycai2@uw.edu>
2023-02-28 19:39:57 -0800

    Install geopandas

--- temp.py
+++ temp.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+
+
+def q1():
+    data = pd.read_csv('./dataset/Occurence_Aedes_aegypti.csv')
+    print(data.columns)
+    print(len(data))
+
+
+if __name__ == '__main__':
+    q1()
