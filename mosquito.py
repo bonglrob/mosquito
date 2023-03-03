e15a5ec7f4840c617057fc6a99aa43eba5a81270
kyokokurihara <kyoko4pi@uw.edu>
2023-03-03 01:08:37 -0800

    updated population datasets and functions (in progress))

--- mosquito.py
+++ mosquito.py
@@ -11,6 +11,13 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 
+def get_path(filename: str) -> str:
+    """
+    This function takes a file name and returns the file path.
+    """
+    return os.path.join('./dataset', filename)
+
+
 def get_df_m(file_path: str) -> pd.DataFrame:
     """
     This function takes a file name (str) of one of mosquito occurence
@@ -51,7 +58,7 @@ def get_df_p(file_path: str) -> pd.DataFrame:
     return data
 
 
-def city_df() -> pd.DataFrame:
+def generate_city_df() -> pd.DataFrame:
     """
     This function returns a dataframe where all datasets on city tempereature
     and precipitation are combined.
@@ -83,17 +90,49 @@ def city_df() -> pd.DataFrame:
     return result
 
 
+def get_df_pop(file_name):
+    data = pd.DataFrame(pd.read_excel(file_name))
+    return data
+
+
+def clean_up_pop_df():
+    # 1947-1970 data
+    pop4769 = get_df_pop(get_path('popca_4769.xlsx'))
+    pop4769 = pop4769.loc[2:59, :]
+    years = [str(i) for i in range(1947, 1971)]
+    columns = ["County", "1940"] + years
+    columns.insert(5, "1950_ap")
+    columns.insert(16, "1960_ap")
+    pop4769.columns = columns
+    pop4769 = pop4769.drop(columns=['1950_ap', '1960_ap', '1970'])
+
+    pop7080 = get_df_pop(get_path('popca_7080.xlsx'))
+    pop7080 = pop7080.loc[:, ['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2']]
+    pop7080.columns = ['County', 'Year', 'Population']
+    print(pop7080)
+    pop_all = pop4769.copy()
+    pop_1970 = []
+    for i in range(58):
+        pop_id = 15 + i * 13
+        pop_1970.append(pop7080.loc[15, 2])
+    print(pop_all)
+    # append a list of 1970 based on county
+    # do the same for all
+    pass
+
+
+
 def main() -> None:
     # read files
-    mosquito1 = get_df_m(os.path.join('./dataset',
-                         'Aedes_aegypti_occurrence.csv'))
-    mosquito2 = get_df_m(os.path.join('./dataset',
-                         'Anopheles_quadrimaculatus_occurrence.csv'))
-    mosquito3 = get_df_m(os.path.join('./dataset',
-                         'Culex_tarsalis_occurrence.csv'))
-    print(mosquito3)
+    # mosquito1 = get_df_m(get_path('Aedes_aegypti_occurrence.csv'))
+    # mosquito2 = get_df_m(get_path('Anopheles_quadrimaculatus_occurrence.csv'))
+    # mosquito3 = get_df_m(get_path('Culex_tarsalis_occurrence.csv'))
+    # print(mosquito3)
 
-    city_data = city_df()
+    # city_data = generate_city_df()
+    clean_up_pop_df()
+
+    
 
     # question 1
 
