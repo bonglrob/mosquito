c8688da76c514c5c2c7e93dd094f63767c3dd451
kyokokurihara <kyoko4pi@uw.edu>
2023-03-02 18:36:35 -0800

    wrote four functions (get_df_m, get_df_t, get_df_p, write_df) to read datasets.

---
+++ mosquito.py
@@ -0,0 +1,100 @@
+"""
+CSE 163 Final Project
+Kyoko Kurihara, Name here?
+
+This file implements functions for mosquito prediction. ...
+"""
+
+import os
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+
+def get_df_m(file_path: str) -> pd.DataFrame:
+    """
+    This function takes a file name (str)
+    and returns the file in dataframe format.
+    This is only for theree mosquito occurence files.
+    """
+    # is there any other way "low_memory=False"
+    data = pd.read_csv(file_path, delimiter='\t', low_memory=False)
+    return data
+
+
+def get_df_t(file_path: str) -> pd.DataFrame:
+    """
+    This function takes a file name (str)
+    and returns the file in dataframe format.
+    This is only for temperature datasets.
+    """
+    data = pd.read_csv(file_path)
+    data.columns = ["Date", "Temp", "Anomaly"]
+    data = data.loc[4:, "Date":"Temp"]
+    return data
+
+
+def get_df_p(file_path: str) -> pd.DataFrame:
+    """
+    This function takes a file name (str)
+    and returns the file in dataframe format.
+    This is only for precipitation datasets.
+    """
+    data = pd.read_csv(file_path)
+    data.columns = ["Date", "Prec", "Anomaly"]
+    data = data.loc[4:, "Date":"Prec"]
+    return data
+
+
+def write_df() -> pd.DataFrame:
+    """
+    This function returns a dataframe where all datasets on
+    city tempereature and precipitation are combined.
+    """
+    cities = ["Eureka", "Fresno", "Los_Angeles", "Sacramento",
+              "San_Diego", "San_Francisco"]
+    result = None
+    for city in cities:
+        # read files
+        temp_path = './dataset/' + city + '_temp.csv'
+        prec_path = './dataset/' + city + '_prec.csv'
+        temp_df = get_df_t(temp_path)
+        prec_df = get_df_p(prec_path)
+
+        # merge files
+        if result is None:
+            result = temp_df.merge(prec_df, left_on="Date",
+                                   right_on="Date", how="outer")
+            result.rename(columns={"Temp": "Temp_" + city,
+                                   "Prec": "Prec_" + city}, inplace=True)
+
+        else:
+            result = result.merge(temp_df, left_on="Date",
+                                  right_on="Date", how="outer")
+            result = result.merge(prec_df, left_on="Date",
+                                  right_on="Date", how="outer")
+            result.rename(columns={"Temp": "Temp_" + city,
+                                   "Prec": "Prec_" + city}, inplace=True)
+    return result
+
+
+def main() -> None:
+    # read files
+    mosquito1 = get_df_m(os.path.join('./dataset',
+                         'Aedes_aegypti_occurrence.csv'))
+    mosquito2 = get_df_m(os.path.join('./dataset',
+                         'Anopheles_quadrimaculatus_occurrence.csv'))
+    mosquito3 = get_df_m(os.path.join('./dataset',
+                         'Culex_tarsalis_occurrence.csv'))
+
+    city_data = write_df()
+
+    # question 1
+
+    # question 2
+
+    # question 3
+
+
+if __name__ == '__main__':
+    main()
