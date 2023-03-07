0ea5a087115a4606944527bc0592476d7158ef81
Christine Cai <ycai2@uw.edu>
2023-03-06 16:51:04 -0800

    Question 1 Draft

--- mosquito.py
+++ mosquito.py
@@ -11,6 +11,8 @@ from shapely.geometry import Point
 import numpy as np
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from typing import List
+from shapely.geometry import Point
 
 
 def get_path(filename: str) -> str:
@@ -157,7 +159,7 @@ def change_style(pop: pd.DataFrame) -> pd.DataFrame:
     return pop_modified
 
 
-def column_name(df: pd.DataFrame) -> list[str]:
+def column_name(df: pd.DataFrame) -> List[str]:
     """
     This takes a dataframe and returns the dataframe with modified column name.
     """
@@ -228,6 +230,15 @@ def get_map():
     pass
 
 
+def filter_occurence_by_30_year(us_map: gpd.GeoDataFrame, occurence: pd.DataFrame, num: str):
+    """
+    Added a column in the given dataFrame that represents the (longitide, latitude) of the occurences
+    in a given year.
+    """
+    coordinates = zip(occurence['decimalLongitude'], occurence['decimalLatitude'])
+    occurence['coordinates' + num] = [Point(lon, lat) for lon, lat in coordinates]
+
+
 def main() -> None:
     # read files
     # mosquito1 = get_df_m(get_path('Aedes_aegypti_occurrence.csv'))
@@ -239,6 +250,50 @@ def main() -> None:
     plt.savefig("test.png")
 
     # question 1
+    occurence_df = pd.read_csv(get_path('Occurence_Aedes_aegypti.csv'))
+    # filter data to only US
+    is_US = occurence_df['countryCode'] == 'US'
+    occurence_df = occurence_df[is_US]
+    # print(occurence_df['individualCount'].dropna())
+
+    # prepare map of US
+    us_map = gpd.read_file(get_path('gz_2010_us_040_00_5m.json'))
+    us_map = us_map[(us_map['NAME'] != 'Alaska') & (us_map['NAME'] != 'Hawaii')]
+
+    # 1904 - 2023
+    btn_04_33 = (occurence_df['year'] >= 1904) & (occurence_df['year'] <= 1933)
+    btn_34_63 = (occurence_df['year'] >= 1934) & (occurence_df['year'] <= 1963)
+    btn_64_93 = (occurence_df['year'] >= 1964) & (occurence_df['year'] <= 1993)
+    btn_94_23 = (occurence_df['year'] >= 1994) & (occurence_df['year'] <= 2023)
+
+    occurence_04_33 = occurence_df[btn_04_33]
+    occurence_34_63 = occurence_df[btn_34_63]
+    occurence_64_93 = occurence_df[btn_64_93]
+    occurence_94_23 = occurence_df[btn_94_23]
+
+    filter_occurence_by_30_year(us_map, occurence_04_33, '1')
+    filter_occurence_by_30_year(us_map, occurence_34_63, '2')
+    filter_occurence_by_30_year(us_map, occurence_64_93, '3')
+    filter_occurence_by_30_year(us_map, occurence_94_23, '4')
+
+    fig, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2, 2, figsize=(20, 10))
+    us_map.plot(color='#EEEEEE', edgecolor='#FFFFFF', ax=ax1)
+    occurence_points = gpd.GeoDataFrame(occurence_04_33, geometry='coordinates1')
+    occurence_points.plot(column='coordinates1', markersize=5, ax=ax1, vmin=0, vmax=1)
+
+    us_map.plot(color='#EEEEEE', edgecolor='#FFFFFF', ax=ax2)
+    occurence_points = gpd.GeoDataFrame(occurence_34_63, geometry='coordinates2')
+    occurence_points.plot(column='coordinates2', markersize=5, ax=ax2, vmin=0, vmax=1)
+
+    us_map.plot(color='#EEEEEE', edgecolor='#FFFFFF', ax=ax3)
+    occurence_points = gpd.GeoDataFrame(occurence_64_93, geometry='coordinates3')
+    occurence_points.plot(column='coordinates3', markersize=5, ax=ax3, vmin=0, vmax=1)
+
+    us_map.plot(color='#EEEEEE', edgecolor='#FFFFFF', ax=ax4)
+    occurence_points = gpd.GeoDataFrame(occurence_94_23, geometry='coordinates4')
+    occurence_points.plot(column='coordinates4', markersize=5, ax=ax4, vmin=0, vmax=1)
+
+    plt.show()
 
     # question 2
 
